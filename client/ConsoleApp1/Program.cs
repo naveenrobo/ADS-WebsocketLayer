947f83f98b3f30862f516c2243cf22f4f90aab76
Naveen <naveenrobotics@live.in>
2019-01-10 11:08:13 +0530

    Initial commit

---
+++ client/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var ws = new WebSocket("ws://localhost:6001/test"))
+                {
+                    ws.OnMessage += (sender, e) =>
+                    {
+                        Console.WriteLine("Message:  " + e.Data);
+                    };
+                    ws.OnError += (sender, e) => {
+                        Console.WriteLine("error: " + e.Message);
+                    };
+                    ws.Connect();
+                    for (var i = 0; i < 10; i++)
+                    {
+                        ws.Send(i.ToString());
+                    }
+
+
+                    Console.ReadKey(true);
+                    
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ server/DotworldService/DotworldService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Timers;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using TwinCAT.Ads;
+
+namespace DotworldService
+{
+    public class ADSClientLayer : WebSocketBehavior
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static TcAdsClient adsClient = new TcAdsClient();
+        private int lrealHandle;
+        private bool error = false;
+
+        public ADSClientLayer()
+        {
+           try
+            {
+                adsClient = new TcAdsClient();
+                adsClient.Connect("192.168.1.8.1.1", 851);
+                lrealHandle = adsClient.CreateVariableHandle("MAIN.M1");
+            }
+            catch(Exception e)
+            {
+                error = true;
+                log.Fatal("Exception while trying to connect to ADS");
+            }
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            try
+            {
+                log.Debug("Received Message from websocket client");
+                log.Info(e.Data);
+                double data;
+                var isDouble = Double.TryParse(e.Data, out data);
+                if (isDouble && !error)
+                {
+                    adsClient.WriteAny(lrealHandle, Double.Parse(e.Data));
+                    log.Info("Data for " + e.Data + " sent to ADS");
+                    Send("Data for " + e.Data + " sent to ADS");
+                }
+                else
+                {
+                    log.Error("Error while sending data");
+                    log.Debug("isDouble? " + isDouble + ", error? " + error);
+                    Send("Not updating data in ads service");
+                }
+            }
+            catch(Exception ex)
+            {
+                log.Fatal("Unable to send message to client");
+                log.Fatal(ex.Message);
+            }
+        }
+    }
+
+    partial class DotworldService : ServiceBase
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public WebSocketServer wssv;
+
+        public DotworldService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("Started Service");
+            StartWebSocket();
+        }
+        protected override void OnStop()
+        {
+            log.Info("Service Stopped");
+            wssv.Stop();
+        }
+
+        protected void StartWebSocket()
+        {
+            try
+            {
+                log.Info("Starting websocket server");
+                wssv = new WebSocketServer("ws://localhost:6001");
+                wssv.AddWebSocketService<ADSClientLayer>("/test");
+                wssv.Start();
+                log.Info("Started websocket server on localhost");
+            }
+            catch (Exception err)
+            {
+                log.Fatal("Error while starting websocket server");
+                log.Error(err.Message);
+            }
+
+        }
+    }
+
+   
+}
